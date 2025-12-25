0492ab9993c85a116915ea59e40bc037892fe274
syktox <markus.kammerstetter@hotmail.com>
2025-12-25 01:32:38 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,10 @@
-#include "MainWindow.h"
-#include <memory>
+#include "ui.h"
 
 int main(int argc, char **argv) {
-    // Instanz der UI erstellen
-    auto ui = MainWindow::create();
-
-    // Initialen Wert für eine Property setzen
-    ui->set_user_name("Syktox");
-
-    // Den Callback (Button-Klick) mit C++ Logik verbinden
-    ui->on_button_clicked([&] {
-        // Aktuellen Wert aus der UI lesen
-        int current_count = ui->get_counter();
-        
-        // Wert erhöhen und zurück an die UI senden
-        ui->set_counter(current_count + 1);
-    });
-
-    // Fenster anzeigen und Event-Loop starten
-    ui->run();
-
+    auto app = App::create();
+    
+    app->run();
+    
     return 0;
 }
+
