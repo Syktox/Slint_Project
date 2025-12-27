42f03b6a9f637ee98fa2ab03384c010454c7ebd8
syktox <markus.kammerstetter@hotmail.com>
2025-12-27 16:47:39 +0100

    logic of quit

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,11 @@ int main(void) {
     HangmanLogic& hLogic ( HangmanLogic::getInstance() );
     std::string secretWord ( hLogic.GetSecretWord(8) );  
 
-    /*
-    App->on_reset_clicked([&] () {
-        hLogic.resetHangman();
-        std::print("Secret Word: {}\n", secretWord);
+    App->on_quit_clicked([&] () {
+        slint::quit_event_loop();
     });
-
-    App->on_start_clicked([&] () {
-        hLogic.resetHangman();
-    });
-    */
+    
+    
    
     App->run();
     return 0;
